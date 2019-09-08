50b83811e5be64b5cf11c4fe52a9f46fa9dcde55
Quang Lam <quang.lam2807@gmail.com>
2019-09-08 10:27:40 -0500

    Fix workspace selector shortcut does not show correctly

--- public/libs/workspaces.js
+++ public/libs/workspaces.js
@@ -129,7 +129,9 @@ const setWorkspacePicture = (id, sourcePicturePath) => {
         pictureId,
         picturePath: destPicturePath,
       });
-      return fsExtra.remove(currentPicturePath);
+      if (currentPicturePath) {
+        return fsExtra.remove(currentPicturePath);
+      }
     })
     .catch(console.log);
 };
