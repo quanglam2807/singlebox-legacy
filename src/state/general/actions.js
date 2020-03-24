8dff0fbf038a355c30a6e319a5675a0f54d78ec3
Quang Lam <quang.lam2807@gmail.com>
2020-03-24 15:18:29 +0700

    remove legacy code

--- src/state/general/actions.js
+++ src/state/general/actions.js
@@ -5,7 +5,6 @@ import {
   UPDATE_CAN_GO_BACK,
   UPDATE_CAN_GO_FORWARD,
   UPDATE_DID_FAIL_LOAD,
-  UPDATE_IS_DARK_MODE,
   UPDATE_IS_DEFAULT_MAIL_CLIENT,
   UPDATE_IS_DEFAULT_WEB_BROWSER,
   UPDATE_IS_FULL_SCREEN,
@@ -51,11 +50,6 @@ export const updateIsDefaultMailClient = (isDefaultMailClient) => ({
 export const updateIsDefaultWebBrowser = (isDefaultWebBrowser) => ({
   type: UPDATE_IS_DEFAULT_WEB_BROWSER,
   isDefaultWebBrowser,
-});
-
-export const updateIsDarkMode = (isDarkMode) => ({
-  type: UPDATE_IS_DARK_MODE,
-  isDarkMode,
 });
 
 export const updateIsLoading = (isLoading) => ({
